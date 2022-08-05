5c37abdf28821f733b9cdccc5c431ae53345f94e
MarcusMix <tradedomarcus@gmail.com>
2022-08-05 16:05:02 -0300

    Updade button hover

--- script/script.js
+++ script/script.js
@@ -1,22 +1,6 @@
 // AOS ANIMATION
   AOS.init();
 
-// DIGITACAO EFEITO
-
-// const saida = document.querySelector(".saida");
-
-// function digitacao(texto, contador) {
-//   if (contador < texto.length) {
-//     setTimeout(() => {
-//       saida.textContent += texto.charAt(contador);
-//       contador++;
-//       digitacao(texto, contador);
-//     }, 89);
-//   }
-// }
-
-// digitacao("E eu sou Desenvolvedor Web", 0);
-
 // VANILLA ANIMATION
 VanillaTilt.init(document.querySelectorAll(".img-projetos"), {
   max: 15,
@@ -27,7 +11,8 @@ VanillaTilt.init(document.querySelectorAll(".icons"), {
   max: 30,
   speed: 600,
 }) 
-// DIGITACAO EFEITO 2
+
+// DIGITACAO EFEITO 
 
 function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split('');
