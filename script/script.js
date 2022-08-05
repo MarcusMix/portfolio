b98c6cb125a678e6d701f1ea7c254a68fdb44136
MarcusMix <tradedomarcus@gmail.com>
2022-08-05 12:36:46 -0300

    Create text animation

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
-<script>
-  AOS.init();
-</script>
+// AOS ANIMATION
+  // AOS.init();
+
+// DIGITACAO EFEITO
+
+const saida = document.querySelector(".saida");
+
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      saida.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 89);
+  }
+}
+
+digitacao("E eu sou Desenvolvedor Web", 0);
