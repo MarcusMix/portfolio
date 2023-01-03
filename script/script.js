c40ce2e69ed8342b51d714b8bfb4fc84278605bf
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-03 18:07:25 -0300

    fix: edit setTimeout for more slowly

--- script/script.js
+++ script/script.js
@@ -18,7 +18,7 @@ function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split('');
   elemento.innerHTML = '';
   textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    setTimeout(() => elemento.innerHTML += letra, 160 * i);
   })
 }
 
